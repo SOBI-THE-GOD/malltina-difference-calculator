c74e61e66f17aa1d31f6684de58699fdfc925e64
SOBI-THE-GOD <doom.of.me803@gmail.com>
2024-11-09 10:25:10 +0330

    inputs added for better ux

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 const $ = document;
-const resetBtn = $.querySelector(".reset-btn");
-const firstResult = $.querySelector(".first-result");
-const secondResult = $.querySelector(".second-result");
+const calcBtn = $.querySelector(".calc-btn");
+const firstLineResult = $.querySelector(".first-result");
+const secondLineResult = $.querySelector(".second-result");
 const calcResultContainer = $.querySelector(".calculation-result");
-
-const newWeight = Number(window.prompt("وزن جدید به گرم"));
-const dollarPrice = Number(window.prompt("قیمت دلار به تومن"));
-const coefficient =
-    newWeight < 1000
-        ? 11
-        : newWeight >= 1000 && newWeight < 5000
-        ? 10
-        : newWeight >= 5000
-        ? 9
-        : 0;
-console.log(coefficient);
-const finalDifference = (
-    ((newWeight / 1000) * (coefficient + 2) + 1) * dollarPrice +
-    46000 -
-    ((0.15 * (coefficient + 2) + 1) * dollarPrice + 46000)
-).toLocaleString("en-US", { maximumFractionDigits: 0 });
-firstResult.innerHTML = `جرم نهایی کالا  »  ${newWeight} گرم`;
-secondResult.innerHTML = `مابه تفاوت وزنی به مبلغ ${finalDifference} تومان پرداخت گردد`;
+const weightInput = $.querySelector("#weight");
+const currencyInput = $.querySelector("#currency");
+let coefficient = null;
+let newWeight = null;
+let currencyPrice = null;
+let finalDifference = null;
+const calcCeofficient = (newWeight) => {
+    if (newWeight < 1000) {
+        return 11;
+    } else if (newWeight >= 1000 && newWeight < 5000) {
+        return 10;
+    } else if (newWeight >= 5000) {
+        return 9;
+    } else {
+        return 0;
+    }
+};
 
 calcResultContainer.onclick = () => {
     navigator.clipboard.writeText(`جرم نهایی کالا  »  ${newWeight} گرم
 مابه تفاوت وزنی به مبلغ ${finalDifference} تومان پرداخت گردد`);
 };
-resetBtn.onclick = () => {
-    window.history.go(0);
+calcBtn.onclick = () => {
+    newWeight = Number(weightInput.value.trim());
+    currencyPrice = Number(currencyInput.value.trim());
+    coefficient = calcCeofficient(newWeight);
+    finalDifference = (
+        ((newWeight / 1000) * (coefficient + 2) + 1) * currencyPrice +
+        46000 -
+        ((0.15 * (coefficient + 2) + 1) * currencyPrice + 46000)
+    ).toLocaleString("en-US", { maximumFractionDigits: 0 });
+    firstLineResult.innerHTML = `جرم نهایی کالا  »  ${newWeight} گرم`;
+    secondLineResult.innerHTML = `مابه تفاوت وزنی به مبلغ ${finalDifference} تومان پرداخت گردد`;
+    weightInput.value = "";
+    currencyInput.value = "";
 };
