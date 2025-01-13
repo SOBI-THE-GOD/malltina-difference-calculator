411a2c594ce73560666d9f6377d017101c2b4f96
SOBI-THE-GOD <doom.of.me803@gmail.com>
2025-01-13 14:04:30 +0330

    another try to deploy

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{jsx, js}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        showup: {
+          "0%": { opacity: "0", transform: "translate(-50%, calc(0.25rem - 50%))" },
+          "100%": { opacity: "1", transform: "translate(-50%, -50%)" }
+        },
+        disapear: {
+          "0%": { opacity: "1", transform: "translate(-50%, 0)" },
+          "100%": { opacity: "0", transform: "translate(-50%, 0.25rem)" },
+        }
+      },
+      animation: {
+        showup: "showup 0.45s ease forwards",
+        disapear: "disapear 0.45s ease"
+      },
+    },
+  },
+  plugins: [],
+}
+
